c0d6176e6bbea7914b121dcaa65d8480771c1ec4
jgonzal3 <julio.gonzalez@ymail.com>
2025-04-29 15:46:37 +0100

    Update Algo3_1.py

--- Chapter3/Algo3_1.py
+++ Chapter3/Algo3_1.py
@@ -2,20 +2,6 @@ import pandas as pd
 import numpy as np
 from tabulate import tabulate
 
-def kron_to_matrix(kron_array):
-    """
-    Converts a Kron array to a matrix of size (NG x 3).
-
-    Parameters:
-        kron_array (numpy.ndarray): Array of size (NG x 3).
-
-    Returns:
-        numpy.ndarray: Matrix of size (NG x 3).
-    """
-    kron_matrix = np.array([[kron_array[0], kron_array[2],0],[kron_array[2],kron_array[1],0],[0,0,kron_array[1]]])
-    return kron_matrix
-
-
 def read_power_file(file_path):
     """
     Reads the file and creates a matrix of size (# of lines x 3).
@@ -34,16 +20,20 @@ def read_power_file(file_path):
         lines = file.readlines()
         for line_number, line in enumerate(lines[:-2], start=1):  # Process all lines except the last
             elements = line.strip().split(',')
-            if len(elements) != 3:
-                raise ValueError(f"Line {line_number} does not have exactly 3 elements: {line.strip()}")
+            if len(elements) != 5:
+                raise ValueError(f"Line {line_number} does not have exactly 5 elements: {line.strip()}")
             if float(elements[0]) == 0:
                 raise ValueError(f"Line {line_number} has the first element as zero: {line.strip()}")
-            matrix.append(list(map(float, elements)))
+            matrix.append(list(map(float, elements[:-2])))
+
+        n2 = len(matrix)*len(matrix)
+        n = len(matrix)
 
         before_last_line = lines[-2].strip().split(',')
-        if len(before_last_line) != 3:
-            raise ValueError(f"Last line does not have exactly 3 elements: {lines[-1].strip()}")
+        if len(before_last_line) != n2:
+            raise ValueError(f"Last line does not have exactly {n} elements: {lines[-1].strip()}")
         kron_array = list(map(float, before_last_line))
+        kron_array = np.array(kron_array).reshape(n,n)
 
         # Process the last line for alfa, epsilon, and iterations
         last_line = lines[-1].strip().split(',')
@@ -99,20 +89,41 @@ def PL(kron_array, power_values):
     Returns:
         float: Total power loss.
     """
-    total_power_loss = kron_array[0] * power_values[0]**2 + kron_array[1] * power_values[1]**2 + 2*kron_array[2]*power_values[0]*power_values[1]
-    return total_power_loss
+
+    total_power = sum(
+        kron_array[i][j] * power_values[i] ** 2 if i == j else power_values[i] * power_values[j] * kron_array[i][j]
+        for i in range(len(power_values))
+        for j in range(len(kron_array))
+    )
+
+    return total_power
+
+def optimal_PG(lambda_value, kron_array, power_values):
+    """
+    Calculate the optimal power generation.
+
+    Parameters:
+        lambda_value (float): The calculated lambda.
+        kron_array (numpy.ndarray): Array of size (NG x 3).
+        power_values (numpy.ndarray): Array of power values for each generator.
+
+    Returns:
+        float: Optimal power generation.
+    """
+    pass
 
 def perform_load_flow(PD,M,kron_array, param, lambda_value,power_values):
     epsilon = param[1]
     alfa = param[0]
-    km = kron_to_matrix(kron_array)
+    Bij = kron_array
 
     optimal_power = [
-        (lambda_value * (1 - 2.0 * power_values[j] * km[i][j]) - M[i][1]) /
-        (2 * (M[i][0] + lambda_value * km[i][i]))
+        sum(
+            (lambda_value * (1 - 2.0 * power_values[j] * Bij[i][j]) - M[i][1]) /
+            (2 * (M[i][0] + lambda_value * Bij[i][i]))
+            for j in range(len(power_values)) if i != j
+        )
         for i in range(len(power_values))
-        for j in range(len(power_values))
-        if i != j
     ]
 
     # Reshape the optimal power array to match the number of generators
@@ -128,15 +139,19 @@ def perform_load_flow(PD,M,kron_array, param, lambda_value,power_values):
 
 count = 0
 converged = False
+
+#Pd = 180
 # Example usage
-file_path = 'power_kron.txt'
+file_path = 'power4.txt'
 M, kron_array, param  = read_power_file(file_path)
-PD = 150
+PD = 160
+#PD = 150
 R=param[2]
 
-# Initialise the algorithm
+
 lambda_value = estimate_lambda(PD, M)
 power_opt = estimate_power(lambda_value, M)
+
 loss = 0.0
 
 while not converged and count < R:
@@ -148,7 +163,9 @@ data = {
     'Iteration': count,
     'Lambda': [lambda_value],
     'P1': [power_opt[0]],
-    'P2': [power_opt[1]]
+    'P2': [power_opt[1]],
+    'P3': [power_opt[2]],
+
 }
 
 # Create the DataFrame
